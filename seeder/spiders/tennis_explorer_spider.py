daa22d8aa71976ef14a4b49c980c903e4a4ec744
Michael Hynes <mike.hynes@protonmail.com>
2022-06-15 08:48:38 -0400

    Added working test

--- seeder/spiders/tennis_explorer_spider.py
+++ seeder/spiders/tennis_explorer_spider.py
@@ -1,11 +1,15 @@
-import os
+from copy import deepcopy
 import logging
+import os
+import re
 
 from datetime import MINYEAR, datetime, timedelta
 
 from urllib.parse import urlparse, parse_qs
 
+from bs4 import BeautifulSoup
 import scrapy
+
 from seeder.items import MatchItem
 
 logger = logging.getLogger(__name__)
@@ -43,7 +47,7 @@ class TennisExplorerSpider(scrapy.Spider):
     endpoint_parsers = {
       '/results/': self.parse_match_listings,
     }
-    url = urlparse(reponse.url)
+    url = urlparse(response.url)
     parser = endpoint_parsers.get(url.path) 
     if parser:
       yield parser(response)
@@ -63,24 +67,197 @@ class TennisExplorerSpider(scrapy.Spider):
       - Requests for listings on other dates
       - Parses match items
     """
-    def _parse_date(url):
-      dt = None
-      try: 
-        qs = parse_qs(urlparse(url).query)
-        y = qs.get('year', [])
-        m = qs.get('month', [])
-        d = qs.get('day', [])
-        if all([len(y) == 1, len(m) == 1, len(d) == 1]):
-          dt = datetime(int(y[0]), int(m[0]), int(d[0]))
-      except Exception as e:
-        self.log.error(f"Encountered exception: '{e}' when parsing url: '{dt}'")
-      finally:
-        return dt
+    soup = BeautifulSoup(response.body, 'html.parser')
+    for record in self._parse_match_records(soup, response.url): 
+      yield record
 
     # Retrieve links to the next day; each daily match summary
     # page has a set of 3 navigation links:
     #   '« previous day', 'today', 'next day »'
     for href in response.css('li.dNav a::attr(href)').getall():
-      if self._is_datetime_bounded(_parse_date(href)):
-        self.log.warning(href)
+      if self._is_datetime_bounded(self._parse_date(href)):
         yield scrapy.Request(response.urljoin(href), self.parse)
+
+  @staticmethod
+  def _parse_match_records(soup, url):
+    """
+    Parse a match results table into an iterable of items.
+    """
+    context = {
+      'match_date': TennisExplorerSpider._parse_date(url), 
+    }
+    records = []
+    tables = soup.find_all('table', class_='result')
+    for tab in tables:
+      records += TennisExplorerSpider._parse_match_table_rows(tab, context)
+    return records
+
+  @staticmethod
+  def _parse_match_table_rows(soup, global_context=None):
+    """
+    Parse rows of table into an iterable of items.
+    """
+    def _build_player_match_record(row, context):
+      record = deepcopy(context)
+      first_score_col = None
+      cols = row.find_all('td')
+      ncols = len(cols)
+      for (k, col) in enumerate(cols, start=1):
+        cls = ' '.join(col.attrs.get('class', []))
+        if cls == 'first time':
+          record['match_time'] = col.next_element
+        elif cls == 't-name':
+          record['player'] = col.find('a').attrs.get('href')
+        elif cls == 'coursew':
+          record['avg_odds_p1'] = col.next_element
+        elif cls == 'course':
+          record['avg_odds_p2'] = col.next_element
+        elif cls == 'result':
+          record['result'] = col.next_element
+        elif cls == 'score':
+          if first_score_col is None:
+            first_score_col = k
+          key = f'score{k - first_score_col + 1}'
+          record[key] = col.next_element
+        elif (cls == '') and (k == ncols):
+          match_url = col.find('a').attrs.get('href')
+          match_id = parse_qs(urlparse(match_url).query).get('id')[0]
+          record['match_id'] = match_id
+      return record
+    
+    def _coerce_record_dtypes(record):
+      fn_map = {
+        'match_time':   coerce_timedelta,
+        'match_id':     coerce_int,
+        'result':       coerce_int,
+        'score1':       coerce_int,
+        'score2':       coerce_int,
+        'score3':       coerce_int,
+        'score4':       coerce_int,
+        'score5':       coerce_int,
+        'avg_odds_p1':  coerce_float,
+        'avg_odds_p2':  coerce_float,
+      }
+      coerced = {}
+      for (key, val) in record.items():
+        coerced[key] = fn_map.get(key, lambda _: _)(val)
+      return coerced
+
+    def _key_record(row):
+      """
+      Retrieve the row's id. Each row is one of a pair, e.g. (10, 10b), that signifies
+      the "home" and "away" counterpart players for the match.
+      """
+      pattern = r'r([0-9]+)(b?)'
+      key = re.match(pattern, row.attrs.get('id', ''))
+      if key:
+        return key.groups()
+      return None
+
+    def _build_records(rows, global_context):
+      records = {}
+      context = {}
+      for (n, row) in enumerate(rows, start=1):
+        # If this row contains a new header, reset the tournament context for the subsequent rows,
+        # and then continue to the next row, which contains match result data. 
+        if ' '.join(row.attrs.get('class', [])) == 'head flags':
+          context = {
+            **global_context,
+            **{'tournament': row.find('td', class_='t-name').find('a').attrs.get('href')},
+          }
+          continue
+        key = _key_record(row)
+        if not key:
+          # logger.warning(f"Could not parse a key from row {n} with text: '{row.text}'")
+          continue
+        raw_record = _build_player_match_record(row, context)
+        records[key] = _coerce_record_dtypes(raw_record)
+      return records
+
+    def _merge_player_records(records):
+      split_fn = lambda k: k[1] == ''
+      left_records = {key[0]: value for (key, value) in records.items() if split_fn(key)}
+      right_records = {key[0]: value for (key, value) in records.items() if not split_fn(key)}
+      merged = []
+      for (key, record) in right_records.items():
+        left = left_records[key]
+        right = right_records[key]
+        merged.append({
+          'match_id':     left['match_id'],
+          'tournament':   left['tournament'],
+          'match_at':     left['match_date'] + left['match_time'],
+
+          'is_win_p1':    left['result'] > right['result'],
+          'is_win_p2':    right['result'] > left['result'],
+
+          'avg_odds_p1':  left['avg_odds_p1'],
+          'avg_odds_p2':  left['avg_odds_p2'],
+
+          'p1':           left['player'],
+          'result_p1':    left['result'],
+          'sets_p1':      sum_ignore_none(*[left[f'score{k}'] for k in range(1, 6)]),
+          'score1_p1':    left['score1'],
+          'score2_p1':    left['score2'],
+          'score3_p1':    left['score3'],
+          'score4_p1':    left['score4'],
+          'score5_p1':    left['score5'],
+
+          'p2':           right['player'],
+          'result_p2':    right['result'],
+          'sets_p2':      sum_ignore_none(*[right[f'score{k}'] for k in range(1, 6)]),
+          'score1_p2':    right['score1'],
+          'score2_p2':    right['score2'],
+          'score3_p2':    right['score3'],
+          'score4_p2':    right['score4'],
+          'score5_p2':    right['score5'],
+        }) 
+      return merged
+
+    rows = soup.find_all('tr', class_=['head flags', 'one', 'two'])
+    context = deepcopy(global_context or {})
+    records = _build_records(rows, context)
+    return _merge_player_records(records)
+    return result #[r for r in records.values()]
+
+  @staticmethod
+  def _parse_date(url):
+    dt = None
+    try: 
+      qs = parse_qs(urlparse(url).query)
+      y = qs.get('year', [])
+      m = qs.get('month', [])
+      d = qs.get('day', [])
+      if all([len(y) == 1, len(m) == 1, len(d) == 1]):
+        dt = datetime(int(y[0]), int(m[0]), int(d[0]))
+    except Exception as e:
+      logger.error(f"Encountered exception: '{e}' when parsing url: '{dt}'")
+    finally:
+      return dt
+
+def sum_ignore_none(*args):
+  result = 0
+  for arg in args:
+    if arg is not None:
+      result += arg
+  return result
+
+def coerce_int(x, default=None):
+  try:
+    return int(x)
+  except ValueError:
+    return default
+
+def coerce_float(x, default=None):
+  try:
+    return float(x)
+  except ValueError:
+    return default
+
+def coerce_timedelta(x, default=timedelta()):
+  result = default
+  pattern = r"([0-2][0-3]):([0-5][0-5])"
+  m = re.match(pattern, x)
+  if m is not None:
+    hours, minutes = m.groups()
+    result = timedelta(hours=groups[0], minutes=groups[11])
+  return result
--- tests/seeder/spiders/test_tennis_explorer_spider.py
+++ tests/seeder/spiders/test_tennis_explorer_spider.py
@@ -33,14 +33,69 @@ class TestTennisExplorerSpider:
   @pytest.mark.vcr()
   def test_parse_match_listings(self):
     base_url = "https://www.tennisexplorer.com/results/?type=all&year={year:04d}&month={month:02d}&day={day:02d}"
-    url = base_url.format(year=2000, month=1, day=8)
+    url = base_url.format(year=2000, month=3, day=19)
     response = scrapy.http.HtmlResponse(url, body=requests.get(url).content)
     spider = TennisExplorerSpider()
     results = list(spider._parse_match_listings(response))
 
+    # Assert the next set of requests are created
     actual_requests = [r.url for r in results if type(r) is scrapy.Request]
     expected_requests = [
-      base_url.format(year=2000, month=1, day=7),
-      base_url.format(year=2000, month=1, day=9),
+      base_url.format(year=2000, month=3, day=18),
+      base_url.format(year=2000, month=3, day=20),
     ]
     assert sorted(actual_requests) == sorted(expected_requests)
+
+    # Assert the items have been collected
+    actual_records = [r for r in results if type(r) is dict]
+    expected_records = [{
+      "tournament":   "/indian-wells/2000/atp-men/",
+      "match_id":     55469, 
+      "match_at":     datetime.datetime(2000, 3, 19),
+      "is_win_p1":    True,
+      "is_win_p2":    False,
+      "avg_odds_p1":  None,
+      "avg_odds_p2":  None,
+      "p1":           "/player/corretja/",
+      "result_p1":    3,
+      "sets_p1":      18,
+      "score1_p1":    6,
+      "score2_p1":    6,
+      "score3_p1":    6,
+      "score4_p1":    None,
+      "score5_p1":    None,
+      "p2":           "/player/enqvist/",
+      "result_p2":    0,
+      "sets_p2":      11,
+      "score1_p2":    4,
+      "score2_p2":    4,
+      "score3_p2":    3,
+      "score4_p2":    None,
+      "score5_p2":    None,
+    }, {
+      "tournament":   "/indian-wells/2000/atp-men/?type=double",
+       "match_id":    55500,
+       "match_at":    datetime.datetime(2000, 3, 19),
+       "is_win_p1":   True,
+       "is_win_p2":   False,
+       "avg_odds_p1": None,
+       "avg_odds_p2": None,
+       "p1":          "/doubles-team/o-brien/palmer/",
+       "result_p1":   2,
+       "sets_p1":     13,
+       "score1_p1":   6,
+       "score2_p1":   7,
+       "score3_p1":   None,
+       "score4_p1":   None,
+       "score5_p1":   None,
+       "p2":          "/doubles-team/haarhuis/stolle/",
+       "result_p2":   0,
+       "sets_p2":     10,
+       "score1_p2":   4,
+       "score2_p2":   6,
+       "score3_p2":   None,
+       "score4_p2":   None,
+       "score5_p2":   None,
+    }]
+    key = lambda r: r['match_id']
+    assert sorted(actual_records, key=key) == sorted(expected_records, key=key)
